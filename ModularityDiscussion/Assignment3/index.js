8d56490c9bb20ba5fe592961b13ecb4171134ff9
SujaiBeniksJ <sujaibeniks.j@rently.com>
2020-02-20 12:29:10 +0530

    added the fix

--- ModularityDiscussion/Assignment3/index.js
+++ ModularityDiscussion/Assignment3/index.js
@@ -1,14 +1,66 @@
-let animal = {
-    eats: "Animal eats"
-  };
-  let rabbit = {
-    jumps: "rabbit eats"
-  };
-  
-  rabbit.__proto__ = animal; // (*)
-  
-console.log("Animal is now a prototype of rabbit so the varibales in animal can be accessed from rabbit");
+class Animal {
+    constructor(name, age){
+        this.name= name;
+        this.age= age;
+    }
 
-console.log(rabbit.eats);
+}
 
-console.log(rabbit.jumps);
+class Human extends Animal{
+    constructor(name, age){
+        super(name, age);
+        this.nooflegs = "";
+    }
+    set legs(legs){
+        this.nooflegs = legs;
+    }
+    get legs() {
+        return this.nooflegs;
+    }
+}
+
+class Bird extends Animal{
+    constructor(name, age){
+        super(name, age);
+        this.noofwings = "";
+    }
+    set wings(wings){
+        this.noofwings = wings;
+    }
+    get wings() {
+        return this.noofwings;
+    }
+}
+
+class Reptiles extends Animal{
+    constructor(name, age){
+        super(name, age);
+        this.nooftail = "";
+    }
+
+    set tails(tails){
+        this.nooftail = tails;
+    }
+    get tails() {
+        return this.nooftail;
+    }
+}
+
+let human1 = new Human("John", 15);
+let bird1 = new Bird("Dove", 20);
+let reptiles1 = new Reptiles("croc", 30);
+
+human1.legs = 20;
+
+bird1.wings = 30;
+
+reptiles1.tails = 40;
+
+console.log("Printing the number of legs wings and reptiles using get method");
+console.log(human1.legs);
+console.log(bird1.legs);
+console.log(reptiles1.legs);
+
+let result = [human1, bird1, reptiles1];
+console.log("Printing the attributes of Human, bird,reptiles in an array");
+console.log(result);
